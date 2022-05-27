c397297b45223266368a3164a0622d696c72086b
eddipietro <erikadipietro7@gmail.com>
2022-05-26 21:21:27 -0300

    Back End

---
+++ BackEnd/src/test/java/com/portfolio/edp/EdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.edp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
