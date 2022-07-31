2e023be752996003b85bc6feb316d2b6f8a3c15e
eddipietro <erikadipietro7@gmail.com>
2022-07-31 01:27:40 -0300

    Arg Programa

---
+++ BackEnd/src/main/java/com/portfolio/edp/Security/Entity/HasSet.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.edp.Security.Entity;
+
+/**
+ *
+ * @author Erika Di Pietro
+ */
+public class HasSet {
+    
+}
---
+++ BackEnd/src/main/java/com/portfolio/edp/Security/Entity/SimpleGrantedAuthority.java
@@ -0,0 +1,5 @@
+package com.portfolio.edp.Security.Entity;
+
+public class SimpleGrantedAuthority {
+
+}
